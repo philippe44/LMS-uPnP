3ba3c9d59a06c9796e7a5d72828ecbf00d58f04d
philippe44 <pgu@9online.fr>
2015-03-21 17:05:33 -0700

    0.2.0.1

--- dev/squeezetiny/output_mr.c
+++ dev/squeezetiny/output_mr.c
@@ -406,10 +406,9 @@ static void output_thru_thread(struct thread_ctx_s *ctx) {
 				u32_t i;
 				u8_t j, *p;
 				p = _buf_readp(ctx->streambuf);
+
 				// 2 or 4 bytes or 3 bytes with no packing, but changed endianness
-				if (out->endianness &&
-					(out->sample_size != 24 ||
-					(out->sample_size == 24 && (ctx->config.L24_format == L24_PACKED || out->channels == 1)))) {
+				if (out->endianness && (out->sample_size != 24 || (out->sample_size == 24 && ctx->config.L24_format == L24_PACKED))) {
 					u8_t buf[4];
 					u8_t inc = out->sample_size/8;
 					space = (space / inc) * inc;
@@ -418,7 +417,40 @@ static void output_thru_thread(struct thread_ctx_s *ctx) {
 						for (j = 0; j < inc; j++) *(p++) = buf[j];
 					}
 				}
-				// 3 bytes with packing required
+#if 0
+				// 3 bytes with packing required, 2 channels
+				if (out->sample_size == 24 && ctx->config.L24_format == L24_PACKED_LPCM) {
+					u8_t buf[12];
+					space = (space / (6*out->channels)) * (6*out->channels);
+					for (i = 0; i < space; i += 6*out->channels) {
+						// order after that should be L0T,L0M,L0B,R0T,R0M,R0B,L1T,L1M,L1B,R1T,R1M,R1B
+						// or order after that should be C0T,C0M,C0B,C1T,C1M,C1B for mono
+						if (out->endianness) for (j = 0; j < 6*out->channels; j += 3) {
+							buf[j] = *(p+j+2);
+							buf[j+1] = *(p+j+1);
+							buf[j+2] = *(p+j);
+						}
+						else for (j = 0; j < 6*out->channels; j++) buf[j] = *(p+j);
+						// L0T,L0M & R0T,R0M or C0T,C0M,C1,C1M for mono
+						*p++ = buf[0]; *p++ = buf[1];
+						*p++ = buf[3]; *p++ = buf[4];
+						if ((out->channels == 2) {
+							// L1T,L1M & R1T,R1M
+							*p++ = buf[6]; *p++ = buf[7];
+							*p++ = buf[9]; *p++ = buf[10];
+						 }
+						// L0B, R0B, L1B, R1B
+						*p++ = buf[2]; *p++ = buf[5];
+						if (out->channels == 2) {
+							*p++ = buf[8]; *p++ = buf[11];
+						}
+						// after that R0T,R0M,L0T,L0M,R1T,R1M,L1T,L1M,R0B,L0B,R1B,L1B
+						// or C0T,C0M,C1T,C1M,C0B,C1B for mono
+					}
+				}
+#endif
+
+				// 3 bytes with packing required, 2 channels
 				if (out->sample_size == 24 && ctx->config.L24_format == L24_PACKED_LPCM && out->channels == 2) {
 					u8_t buf[12];
 					space = (space / 12) * 12;
@@ -428,9 +460,9 @@ static void output_thru_thread(struct thread_ctx_s *ctx) {
 							buf[j] = *(p+j+2);
 							buf[j+1] = *(p+j+1);
 							buf[j+2] = *(p+j);
-                        }
+						}
 						else for (j = 0; j < 12; j++) buf[j] = *(p+j);
- 						// L0T,L0M & R0T,R0M
+						// L0T,L0M & R0T,R0M
 						*p++ = buf[0]; *p++ = buf[1];
 						*p++ = buf[3]; *p++ = buf[4];
 						// L1T,L1M & R1T,R1M
@@ -441,6 +473,28 @@ static void output_thru_thread(struct thread_ctx_s *ctx) {
 						// after that R0T,R0M,L0T,L0M,R1T,R1M,L1T,L1M,R0B,L0B,R1B,L1B
 					}
 				}
+
+				// 3 bytes with packing required
+				if (out->sample_size == 24 && ctx->config.L24_format == L24_PACKED_LPCM && out->channels == 1) {
+					u8_t buf[6];
+					space = (space / 6) * 6;
+					for (i = 0; i < space; i += 6) {
+						// order after that should be C0T,C0M,C0B,C1T,C1M,C1B
+						if (out->endianness) for (j = 0; j < 6; j += 3) {
+							buf[j] = *(p+j+2);
+							buf[j+1] = *(p+j+1);
+							buf[j+2] = *(p+j);
+						}
+						else for (j = 0; j < 6; j++) buf[j] = *(p+j);
+						// C0T,C0M,C1,C1M
+						*p++ = buf[0]; *p++ = buf[1];
+						*p++ = buf[3]; *p++ = buf[4];
+						// C0B, C1B
+						*p++ = buf[2]; *p++ = buf[5];
+						// after that C0T,C0M,C1T,C1M,C0B,C1B
+					}
+				}
+
 			}
 
 			if (!strcmp(out->ext, "wav")) {
@@ -469,8 +523,7 @@ static void output_thru_thread(struct thread_ctx_s *ctx) {
 		} else sleep_time = 100000;
 
 		// all done, time to close the file
-		if (out->write_file && ctx->stream.state <= DISCONNECT && (!_buf_used(ctx->streambuf) || (out->sample_size == 24 && _buf_used(ctx->streambuf) < 12)))
-		{
+		if (out->write_file && ctx->stream.state <= DISCONNECT && (!_buf_used(ctx->streambuf) || (out->sample_size == 24 && _buf_used(ctx->streambuf) < 6*out->channels))) {
 			LOG_INFO("[%p] wrote total %Ld", ctx, out->write_count_t);
 			fclose(out->write_file);
 			out->write_file = NULL;
